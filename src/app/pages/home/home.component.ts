7ad2acbdcd6974ea71d631b5c155f300c943272a
Roman-code-web <selenerg.275@gmail.com>
2023-01-10 15:16:25 -0500

    logica terminada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-titulo="Actividad 15";
+titulo="Actividad 14";
 subtitulo="Comunicación entre componentes padre e hijo en Angular";
 tituloForm="Registro de Personas";
+//variables 
+nombre!:string;
+dni!:string;
+estadocivil="";
+RegistroPersona:any=[]=[];
+//funcion que envia datos;
+enviarDatos(){
+this.RegistroPersona=[{'nombre':this.nombre, 'dni':this.dni,'estadocivil':this.estadocivil}];
+console.log(this.RegistroPersona);
+}
 }
